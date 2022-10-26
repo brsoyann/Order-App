a482fcd750a44fa95c2f0b97b1a9b6ea2c47d567
tatevikbrsoyan <tatevik.brsoyan.y@tumo.org>
2022-10-26 21:37:58 +0400

    Initial Commit

---
+++ Order App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Order App
+//
+//  Created by Tatevik Brsoyan on 26.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
